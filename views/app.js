299cd979852c4ba7900453c8038850c5c5dcfcdc
Abby Sepple <abby.sepple@gmail.com>
2017-07-13 08:47:26 -0500

    server2

--- views/app.js
+++ views/app.js
@@ -1,4 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 var port = process.env.PORT || 6000;
 
 // serve static files
 app.use(express.static(path.resolve('./server/public')));
+
+app.listen(port, function() {
+  console.log("server running, check localhost:6000");
+});
